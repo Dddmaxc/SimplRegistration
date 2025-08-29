78c2c83d664fb47211e50af941c4582868d73d9a
Itachi <navigremory.666@gmail.com>
2025-08-29 13:37:37 +0200

    1

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // <-- импорт HashRouter
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import "./firebase"
+import "./firebase";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
